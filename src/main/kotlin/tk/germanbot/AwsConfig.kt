1c1d75119f9a666bd0360015a90603e9108e84bf
marina <m.chernyavska@gmail.com>
2018-01-01 21:04:48 +0100

    extending repository testing

--- src/main/kotlin/tk/germanbot/AwsConfig.kt
+++ src/main/kotlin/tk/germanbot/AwsConfig.kt
@@ -1,9 +1,8 @@
 package tk.germanbot
 
-import com.amazonaws.auth.AWSCredentials
-import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.client.builder.AwsClientBuilder
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.context.properties.ConfigurationProperties
@@ -11,11 +10,13 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
+import org.springframework.core.env.Environment
 import org.springframework.util.StringUtils
 
 
 @ConfigurationProperties(prefix = "aws.dynamodb")
 class AwsProperties {
+    var region: String = ""
     var endpoint: String = ""
     var accessKey: String = ""
     var secretKey: String = ""
@@ -23,23 +24,24 @@ class AwsProperties {
 
 @Configuration
 @EnableConfigurationProperties(AwsProperties::class)
-@Profile("default", "dev", "prod")
-class AwsConfig(@Autowired val props: AwsProperties) {
+@Profile("!test")
+class AwsConfig(@Autowired val props: AwsProperties,
+                @Autowired val environment: Environment) {
     private val logger = LoggerFactory.getLogger(AwsConfig::class.java)
 
     @Bean
     fun amazonDynamoDB(): AmazonDynamoDB {
-        val amazonDynamoDB = AmazonDynamoDBClient(amazonAWSCredentials())
+        val builder = AmazonDynamoDBClientBuilder.standard()
         if (!StringUtils.isEmpty(props.endpoint)) {
-            logger.debug("Initializing AWS Dynamo db with endpoint {}, access key {}, secret {}",
-                    props.endpoint, props.accessKey, props.secretKey)
-            amazonDynamoDB.setEndpoint(props.endpoint)
+            if (environment.activeProfiles.contains("dev")) {
+                builder.setEndpointConfiguration(
+                        AwsClientBuilder.EndpointConfiguration(props.endpoint, props.region))
+            } else {
+                builder.region = props.region
+            }
+            logger.debug("Initializing AWS Dynamo db with endpoint {}, region {}, access key {}, secret {}",
+                    props.endpoint, props.region, props.accessKey, props.secretKey)
         }
-        return amazonDynamoDB
-    }
-
-    @Bean
-    fun amazonAWSCredentials(): AWSCredentials {
-        return BasicAWSCredentials(props.accessKey, props.secretKey)
+        return builder.build()
     }
 }
--- src/test/kotlin/tk/germanbot/data/TranslationRepositoryIntegrationTest.kt
+++ src/test/kotlin/tk/germanbot/data/TranslationRepositoryIntegrationTest.kt
@@ -15,6 +15,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.context.web.WebAppConfiguration
 import tk.germanbot.Application
 import tk.germanbot.IntegrationTestsConfig
+import java.util.concurrent.atomic.AtomicBoolean
 
 
 /***
@@ -27,35 +28,59 @@ import tk.germanbot.IntegrationTestsConfig
 @Import(IntegrationTestsConfig::class)
 @ActiveProfiles("test")
 class TranslationRepositoryIntegrationTest {
-    private val EXPECTED_GE = "Hallo"
+    private val EXPECTED_DE = "Hallo"
     private val EXPECTED_EN = "Hello"
 
     private var dynamoDBMapper: DynamoDBMapper? = null
     @Autowired
-    private val amazonDynamoDB: AmazonDynamoDB? = null
+    private val db: AmazonDynamoDB? = null
     @Autowired
     internal var repository: TranslationRepository? = null
+
+    companion object {
+        private var tableCreated: AtomicBoolean = AtomicBoolean(false)
+    }
 
     @Before
     @Throws(Exception::class)
     fun setup() {
-        dynamoDBMapper = DynamoDBMapper(amazonDynamoDB)
-        val tableRequest = dynamoDBMapper!!
-                .generateCreateTableRequest(Translation::class.java)
-        tableRequest.provisionedThroughput = ProvisionedThroughput(1L, 1L)
-        amazonDynamoDB!!.createTable(tableRequest)
-
+        dynamoDBMapper = DynamoDBMapper(db)
+        if (!tableCreated.get()) {
+            val tableRequest = dynamoDBMapper!!
+                    .generateCreateTableRequest(Translation::class.java)
+            tableRequest.provisionedThroughput = ProvisionedThroughput(1L, 1L)
+            db!!.createTable(tableRequest)
+            tableCreated.set(true)
+        }
         dynamoDBMapper!!.batchDelete(repository!!.findAll())
+
+        val hello = Translation(de = EXPECTED_DE, en = EXPECTED_EN)
+        repository?.save(hello)
     }
 
     @Test
-    fun sampleTestCase() {
-        val hello = Translation("123", EXPECTED_GE, EXPECTED_EN)
-        repository?.save(hello)
-
-        val result = repository!!.findAll() as List<Translation>
+    fun findAll() {
+        val result = repository!!.findAll()
         assertTrue("Not empty", result.isNotEmpty())
         assertTrue("Contains item with expected translation",
                 result[0].en == EXPECTED_EN)
+    }
+
+    @Test
+    fun findByEnglish() {
+        val result = repository!!.findByEn(EXPECTED_EN)
+        assertTrue("Not empty", result.isNotEmpty())
+        assertTrue("ID Not empty", result[0].id != null)
+        assertTrue("Contains item with expected translation",
+                result[0].de == EXPECTED_DE)
+    }
+
+    @Test
+    fun findByGerman() {
+        val result = repository!!.findByDe(EXPECTED_DE)
+        assertTrue("Not empty", result.isNotEmpty())
+        assertTrue("ID Not empty", result[0].id != null)
+        assertTrue("Contains item with expected translation",
+                result[0].de == EXPECTED_DE)
     }
 }
